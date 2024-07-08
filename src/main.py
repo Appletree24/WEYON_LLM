c2c60cd54390853ee5c11410c0636ba7726b612c
Appletree24 <1246908638zxc@gmail.com>
2024-07-08 18:05:13 +0800

    #feat. Add module to Get Log

--- src/main.py
+++ src/main.py
@@ -30,6 +30,23 @@ def simple_chain(message, history):
         yield partial_message
 
 
+def simple_rag(message, history):
+    chain: Runnable = default_context['simple_rag']
+    partial_message = ""
+    # 历史对话总是从用户开始，然后机器人
+    history_msg = []
+    for i, (user_message, bot_message) in enumerate(history):
+        if isinstance(user_message, list):
+            user_message = "".join(user_message)
+        history_msg.append(('user', user_message))
+        if isinstance(bot_message, list):
+            bot_message = "".join(bot_message)
+        history_msg.append(('ai', bot_message))
+    history_msg = str(history_msg)
+    logs.get_logger('chat').debug(history_msg)
+    prompt = chain.invoke([message, history_msg])
+    return str(prompt)
+
 agent = FayAgentCore()
 
 
@@ -42,12 +59,14 @@ import gradio as gr
 
 if __name__ == "__main__":
     chain_interface = gr.ChatInterface(simple_chain, title="Simple Chain")
+    rag_interface = gr.ChatInterface(simple_rag, title="Simple Rag")
     agent_interface = gr.ChatInterface(simple_agent, title="Simple Agent")
     from fastapi import FastAPI
 
     app = FastAPI()
     gr.mount_gradio_app(app, chain_interface, path="/chain")
     gr.mount_gradio_app(app, agent_interface, path="/agent")
+    gr.mount_gradio_app(app, rag_interface, path="/rag")
     import uvicorn
 
     uvicorn.run(app, host="0.0.0.0", port=7860)
