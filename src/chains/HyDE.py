bf276a1f4961e1497040579ae0743228f48796da
LeoTan2004 <3509836544@qq.com>
2024-08-13 11:09:36 +0800

    #feat.HyDE 假设文档对接Retriever
    将假设文档的结果传递给retriever查询，查询效果有所改善，但是仍然存在问题。当HyDE的结果过长的时候，匹配效果并不好。
    Tested-by: LeoTan2004 <3509836544@qq.com>

--- src/chains/HyDE.py
+++ src/chains/HyDE.py
@@ -1,13 +1,12 @@
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 from pydantic import BaseModel, Field
 
 import chains
 from llm import chat_openai
 from logs import get_logger
-from retriever import qdrant_retriever, doc_retriever
-
-_ = chat_openai, qdrant_retriever, doc_retriever
+from retriever import doc_retriever
 
 logger = get_logger(__name__)
 
@@ -27,10 +26,21 @@ template = """
 
 @chains.register
 def hy_de(ServeChatModel):
-    chat_model = ServeChatModel
+    chat_model: chat_openai.ChatOpenAI = ServeChatModel
     hyde_parser = PydanticOutputParser(pydantic_object=HypotheticalDocument)
     prompt_template = PromptTemplate(template=template,
                                      input_variables=["question"],
                                      partial_variables={
                                          'output_format': hyde_parser.get_format_instructions()})
     return prompt_template | ServeChatModel | hyde_parser
+
+
+def log(p: HypotheticalDocument):
+    logger.debug(p)
+    return p.document
+
+
+@chains.register
+def hyde_rag(hy_de, DocRetriever):
+    retriever: doc_retriever.BaseRetriever = DocRetriever
+    return hy_de | RunnableLambda(lambda x: log(x)) | retriever
--- tests/test_HyDE.py
+++ tests/test_HyDE.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 
+from langchain_core.documents import Document
 from langchain_core.runnables import Runnable
 
 from basic import default_context
@@ -13,3 +14,10 @@ class Test(TestCase):
         test_chain: Runnable = default_context.get_bean('hy_de')
         res = test_chain.invoke({'question': '2023年长沙理工大学的就业情况'})
         self.assertIsInstance(obj=res, cls=HyDE.HypotheticalDocument)
+
+    def test_hyde_rag(self):
+        test_chain: Runnable = default_context.get_bean('hyde_rag')
+        res: list[Document] = test_chain.invoke({'question': '2023年长沙理工大学的就业情况'})
+        self.assertIsNotNone(res)
+        for i, re in enumerate(res):
+            print(f"第{i}篇文档：\n{re.page_content}\n")
